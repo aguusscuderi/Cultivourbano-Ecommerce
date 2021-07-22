d9f9674d5fec01054b6ad5c4856b653b43299149
aguusscuderi <aguusscuderi9@gmail.com>
2021-07-22 09:47:31 -0300

    probando commits

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Holaaa!
+          Esto sera un Ecommerce!
         </p>
         <a
           className="App-link"
